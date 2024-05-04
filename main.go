5ee4b2c045eae3e293b837fc15da26ac5dcc3ab2
DerrickKirimi <derrickkiriimi@gmail.com>
2024-05-04 18:09:20 +0300

    Read data from request body, write with rw

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 	//Handlefunc adds func to defaultServeMux
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
 		log.Println("Ssup G")
+		d, _ := io.ReadAll(r.Body)
+		fmt.Fprintf(rw, "Hello %s\n", d)
 	})
 
 	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
