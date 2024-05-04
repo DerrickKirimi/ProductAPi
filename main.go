4a8c8ce95323425efcef23799438d97fde84b8ba
DerrickKirimi <derrickkiriimi@gmail.com>
2024-05-04 23:44:42 +0300

    Idiomatic Go: Handler

--- main.go
+++ main.go
@@ -1,26 +1,18 @@
 package main
 
 import (
-	"fmt"
-	"io"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/DerrickKirimi/ProductAPi/handlers"
 )
 
 func main() {
 	//Handlefunc adds func to defaultServeMux
-	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
-		log.Println("Ssup G")
-		d, err := io.ReadAll(r.Body)
-		if err != nil{
-			http.Error(rw, "Oops", http.StatusBadRequest)
-			return //Needed as Error does not end the request
-		}
-		fmt.Fprintf(rw, "Hello %s\n", d)
-	})
-
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request){
-		log.Println("Aloha")
-	})
-	http.ListenAndServe(":9090", nil)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	h := handlers.NewHello(l)
+	sm :=  http.NewServeMux()
+	sm.Handle("/", h)
+	http.ListenAndServe(":9090", sm)
 }
