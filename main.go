57e669e46c34acb93f36bd31118e9cecc7e27fec
DerrickKirimi <derrickkiriimi@gmail.com>
2024-05-18 11:16:03 +0300

    Server: Finer control over timeouts, (Addr, handler)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/DerrickKirimi/ProductAPi/handlers"
 )
@@ -13,8 +14,18 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	h := handlers.NewHello(l)
 	g := handlers.NewGoodbye(l)
+
 	sm :=  http.NewServeMux()
 	sm.Handle("/", h)
 	sm.Handle("/goodbye", g)
-	http.ListenAndServe(":9090", sm)
+
+	s := &http.Server{
+		Addr:			":9090",
+		Handler:		sm,
+		IdleTimeout:	120 * time.Second,
+		ReadTimeout:	1 * time.Second,
+		WriteTimeout:	1 * time.Second,
+	}
+
+	s.ListenAndServe()
 }
